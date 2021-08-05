327260b601d176d0e3b5a11619a896be6b538d50
jakob <jakob.germano@gmail.com>
2021-08-04 19:41:47 -0600

    commit tree 1e25fd7bc3e2c5595a50c7d82730856f9c999ccc

--- src/__tests__/Header.test.js
+++ src/__tests__/Header.test.js
@@ -4,8 +4,8 @@ import React from "react";
 
 import Header from "../components/Header";
 
-test("displays the text 'hello from the Header!'", () => {
+test("displays the text 'please pass this test'", () => {
   render(<Header />);
 
-  expect(screen.queryByText("hello from the Header!")).toBeInTheDocument();
+  expect(screen.queryByText("please pass this test"))
 });
